75546221b95ded8954fbbc0a50f99715f7824653
Irfan Zulkarnain <irfanzulkarnain.dev@yahoo.com>
2023-06-08 15:09:17 +0800

    feat: add new routes

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,13 @@ Route::controller(PageController::class)->as('web.')->group(function () {
     Route::get('/', 'home')->name('home');
     Route::get('/contact-us', 'contactUs')->name('contact-us');
 });
+Route::controller(PageController::class)->prefix('private-banking')->as('web.private-banking.')->group(function () {
+    Route::get('/the-partnership', '')->name('the-partnership');
+});
+Route::controller(PageController::class)->prefix('our-firm')->as('web.our-firm.')->group(function () {
+    Route::get('/the-partnership', '')->name('the-partnership');
+    Route::get('/our-mission-and-profile', '')->name('our-mission-and-profile');
+    Route::get('/sustainability', '')->name('sustainability');
+    Route::get('/philanthropy', '')->name('philanthropy');
+    Route::get('/policies-and-disclosure', '')->name('policies-and-disclosure');
+});
