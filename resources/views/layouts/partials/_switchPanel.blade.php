a8eb87e7f1c4eb1e485da549e155411ffc91c5f0
Irfan Zulkarnain <irfanzulkarnain.dev@yahoo.com>
2023-06-15 11:46:23 +0800

    style: update theme color to be blue

--- resources/views/layouts/partials/_switchPanel.blade.php
+++ resources/views/layouts/partials/_switchPanel.blade.php
@@ -2,14 +2,14 @@
     <div class="content-switcher">
         <p>Color Options:</p>
         <ul class="header">
+            <li><a href="#" onClick="setActiveStyleSheet('blue'); return false;" class="button color switch"
+                style="background-color:#3498db"></a></li>
             <li><a href="#" onClick="setActiveStyleSheet('orange'); return false;" class="button color switch"
                     style="background-color:#d35400"></a></li>
             <li><a href="#" onClick="setActiveStyleSheet('green'); return false;" class="button color switch"
                     style="background-color:#2ecc71"></a></li>
             <li><a href="#" onClick="setActiveStyleSheet('red'); return false;" class="button color switch"
                     style="background-color:#e74c3c"></a></li>
-            <li><a href="#" onClick="setActiveStyleSheet('blue'); return false;" class="button color switch"
-                    style="background-color:#3498db"></a></li>
             <li><a href="#" onClick="setActiveStyleSheet('yellow'); return false;" class="button color switch"
                     style="background-color:#f1c40f"></a></li>
         </ul>
