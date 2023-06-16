5cc43f4077f32b9aa04d6e80472192fb951d95b9
Irfan Zulkarnain <irfanzulkarnain.dev@yahoo.com>
2023-06-16 18:32:54 +0800

    style: add sidebar for careers

---
+++ resources/views/web/careers/_sidebar.blade.php
@@ -0,0 +1,114 @@
+<style>
+    /* Styles for the sidebar */
+    #sidebar {
+        font-family: 'OpenSans-Light-webfont', sans-serif;
+        background-color: #333333;
+        padding: 10px;
+    }
+
+    #sidebar h5 {
+        padding: 10px;
+        color: #ffffff;
+        /* White text color */
+    }
+    #sidebar h6 {
+        text-align: left;
+        padding: 10px;
+        color: #ffffff;
+        /* White text color */
+    }
+
+    #sidebar hr {
+        border-color: white;
+    }
+
+    #sidebar ul {
+        padding: 10px;
+        list-style: none;
+        margin: 0;
+    }
+
+    #sidebar ul li {
+        margin-bottom: 5px;
+    }
+
+    #sidebar ul li a {
+        font-size: 14px;
+        color: #ffffff;
+        /* White text color */
+        text-decoration: none;
+    }
+
+    #sidebar ul li a:hover {
+        text-decoration: underline;
+    }
+
+    #sidebar ul ul {
+        padding: 10px;
+        margin-top: 5px;
+    }
+
+    #sidebar ul ul li {
+        margin-bottom: 5px;
+    }
+
+    /* Nested list indicators */
+    #sidebar ul ul li:before {
+        content: "-";
+        margin-right: 5px;
+        color: #ffffff;
+        /* White text color */
+    }
+</style>
+
+<div id="sidebar">
+    <h5>Careers</h5>
+    <hr>
+    <ul>
+        <li><a href="javascript:goTo('whatWeLookFor')">What We Look For</a>
+            <ul>
+                <li><a href="javascript:goTo('undergraduateInternships')">Undergraduate Internships</a></li>
+                <li><a href="javascript:goTo('entryLevelProfessionals')">Entry Level Professionals</a></li>
+                <li><a href="javascript:goTo('experiencedProfessionals')">Experienced Professionals</a></li>
+                <li><a href="javascript:goTo('restart')">Restart@</a></li>
+            </ul>
+        </li>
+        <li><a href="javascript:goTo('totalRewards')">Total Rewards</a>
+            <ul>
+                <li><a href="javascript:goTo('benefitsWellnessAndCompensation')">Benefits, Wellness and Compensation</a></li>
+                <ul>
+                    <li><a href="javascript:goTo('thrivePhysically')">Defining</a></li>
+                    <li><a href="javascript:goTo('thriveFinancially')">Building</a></li>
+                    <li><a href="javascript:goTo('thrivePersonally')">Succession</a></li>
+                </ul>
+                <li><a href="javascript:goTo('careerManagement')">Career Management</a></li>
+                <li><a href="javascript:goTo('ongoingLearning')">Ongoing Learning</a></li>
+                <ul>
+                    <li><a href="javascript:goTo('signaturePrograms')">Signature Programs</a></li>
+                </ul>
+                <li><a href="javascript:goTo('mobility')">Mobility</a></li>
+
+            </ul>
+        </li>
+        <li><a href="javascript:goTo('lifeHere')">Life Here</a>
+            <ul>
+                <li><a href="javascript:goTo('ourCulture')">Our Culture</a></li>
+                <li><a href="javascript:goTo('coreValues')">Core Values</a></li>
+                <li><a href="javascript:goTo('affinityNetworkCommunity')">Affinity Network Community</a></li>
+                <ul>
+                    <li><a href="javascript:goTo('asiaLinkNetwork')">Asia Link Network</a></li>
+                    <li><a href="javascript:goTo('blackAffinityNetwork')">Black Affinity Network</a></li>
+                    <li><a href="javascript:goTo('diverseAbilityAlliesNetwork')">Diverse Ability Allies Network</a></li>
+                    <li><a href="javascript:goTo('globalEmployeeNetwork')">Global Employee Network</a></li>
+                    <li><a href="javascript:goTo('latinoCulturalNetwork')">Latino Cultural Network</a></li>
+                    <li><a href="javascript:goTo('militaryVeteransNetwork')">Military Veterans Network</a></li>
+                    <li><a href="javascript:goTo('parentingNetwork')">Parenting Network</a></li>
+                    <li><a href="javascript:goTo('prideNetwork')">Pride Network</a></li>
+                    <li><a href="javascript:goTo('womensNetwork')">Women's Network</a></li>
+                    <li><a href="javascript:goTo('youngProfessionalsNetwork')">Young Professionals Network</a></li>
+                </ul>
+                <li><a href="javascript:goTo('cares')">Cares</a></li>
+            </ul>
+        </li>
+    </ul>
+</div>
