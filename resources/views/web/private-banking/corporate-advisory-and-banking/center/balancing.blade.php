aeb71d3c12a21cb6a9d8bf55118552c95279d80d
Irfan Zulkarnain <irfanzulkarnain.dev@yahoo.com>
2023-06-22 10:50:04 +0800

    style: add content and hyperlinks

--- resources/views/web/private-banking/corporate-advisory-and-banking/center/balancing.blade.php
+++ resources/views/web/private-banking/corporate-advisory-and-banking/center/balancing.blade.php
@@ -23,10 +23,57 @@
             &nbsp;&nbsp;&nbsp;&bull;&nbsp;What mediation techniques have been developed for conflict resolution?<br><br>
             &nbsp;&nbsp;&nbsp;&bull;&nbsp;What steps has the family taken to engage third parties in order to facilitate conflict resolution and enhance communication and decision-making?<br><br>
         </p>
-        <h2 style="padding-top: 10px;">Insights</h2>
-        <p style="padding-top: 10px;">
-
-        </p>
+        <div style="padding-top: 30px;">
+            <h2 style="padding-bottom: 20px;">Insights</h2>
+            <hr>
+            <div class="two columns">
+                <img src="{{ asset('images/affinity/spring-festival-cover.jpg') }}" alt=""
+                    width="75%" height="auto">
+            </div>
+            <div class="eight columns">
+                <h6 style="text-align: left; padding-bottom: 10px;">The Dynamics of Leadership in a Private Business: A Conversation with Kerry Sulkowicz, M.D., Founder of the Boswell Group</h6>
+                <p>In the feature article of this issue of Owner to Owner, we sit down with Kerry Sulkowicz, M.D., the founder of the Boswell Group, to discuss the challenges leaders face, the complexities of leadership in private and family businesses and the common qualities he has seen in the best leaders.
+                </p>
+            </div>
+        </div>
+        <div style="padding-top: 30px;">
+            <hr>
+            <div class="two columns">
+                <img src="{{ asset('images/affinity/diwali.jpg') }}" alt=""
+                    width="75%" height="auto">
+            </div>
+            <div class="eight columns">
+                <h6 style="text-align: left; padding-bottom: 10px;">Setting Up the Next Generation of Family Business Leaders for Success: A Conversation with Michael Preston, Co-Director of the Columbia Business School Family Business Program</h6>
+                <p>We sit down with Michael Preston, an adjunct professor at Columbia Business School and the co-director of its Family Business Program, to hear insights and advice gleaned from teaching the next generation of family business owners for more than two decades.
+                </p>
+            </div>
+        </div>
+        <div style="padding-top: 30px;">
+            <hr>
+            <div class="two columns">
+                <img src="{{ asset('images/affinity/asia-link.jpg') }}" alt=""
+                    width="75%" height="auto">
+            </div>
+            <div class="eight columns">
+                <h6 style="text-align: left; padding-bottom: 10px;">Why Decision-Making Is So Complicated in a Family Business
+                </h6>
+                <p>We speak with Ellen Perry, a Senior Advisor to WAML, to discuss the complexities around decision-making in a family business, how families can overcome decision blockers and the tools families can use to foster better decision-making.
+                </p>
+            </div>
+        </div>
+        <div style="padding-top: 30px;">
+            <hr>
+            <div class="two columns">
+                <img src="{{ asset('images/affinity/asia-link.jpg') }}" alt=""
+                    width="75%" height="auto">
+            </div>
+            <div class="eight columns">
+                <h6 style="text-align: left; padding-bottom: 10px;">Balancing Business Reinvestment and Shareholder Liquidity Needs in a Multigenerational Family Business
+                </h6>
+                <p>John Secor, Ajit George and Kyle Gordon discuss a key issue faced by family businesses: the difficulty of managing capital and liquidity demands. They zero in on a critical factor of finding the right balance – patient capital – and provide advice on how family businesses can achieve this important objective.
+                </p>
+            </div>
+        </div>
     </div>
     <div class="five columns">
         @include('web.private-banking._sidebar')
--- resources/views/web/private-banking/corporate-advisory-and-banking/center_for_family_business.blade.php
+++ resources/views/web/private-banking/corporate-advisory-and-banking/center_for_family_business.blade.php
@@ -21,6 +21,55 @@
             The mission of the Centre for Family Business is to assist family businesses and their proprietors in achieving their goals and addressing issues at the intersection of family and business. We provide family businesses with advice, insights, and resources on a variety of topics pertaining to family business ownership, based on our years of developing profound subject matter expertise and stewarding an institution that began as a family business more than two centuries ago. Among our primary concerns are the following:
             <br><br>
         </p>
+        <hr>
+        <div style="padding: 10px">
+            <p style="text-align: left; padding-bottom: 10px; font-weight: bold">
+                <a href="javascript:goTo('definingLegacyAndTheFutureOfTheBusiness')">Defining Legacy and the Future of the Business</a></p>
+            <p>Learn more about how we help our clients define their visions, preserve their legacies, shape the future of their businesses and develop their objectives with their families.
+            </p>
+        </div>
+        <hr>
+        <div style="padding: 10px">
+            <p style="text-align: left; padding-bottom: 10px; font-weight: bold"><a href="javascript:goTo('buildingASupportTeam')">Building a Support Team</a></p>
+            <p>Learn more about how we help our clients identify, assess and enhance their teams of trusted advisors.
+            </p>
+        </div>
+        <hr>
+        <div style="padding: 10px">
+            <p style="text-align: left; padding-bottom: 10px; font-weight: bold"><a href="javascript:goTo('successionPlanningManagementAndOwnership')">Succession Planning – Management and Ownership</a></p>
+            <p>Learn more about how we help our clients prepare and plan for management and ownership succession.
+            </p>
+        </div>
+        <hr>
+        <div style="padding: 10px">
+            <p style="text-align: left; padding-bottom: 10px; font-weight: bold"><a href="javascript:goTo('capitalPolicies')">Capital Policies</a></p>
+            <p>Learn more about how we help our clients determine capital policies that balance the needs of the business and the family.
+            </p>
+        </div>
+        <hr>
+        <div style="padding: 10px">
+            <p style="text-align: left; padding-bottom: 10px; font-weight: bold"><a href="javascript:goTo('employmentPolicies')">Employment Policies</a></p>
+            <p>Learn more about how we help our clients develop employment policies for family members.
+            </p>
+        </div>
+        <hr>
+        <div style="padding: 10px">
+            <p style="text-align: left; padding-bottom: 10px; font-weight: bold"><a href="javascript:goTo('governanceAndCommunications')">Governance and Communications</a></p>
+            <p>Learn more about how we help our clients create and enhance formal governance and communication structures for their companies and family members.
+            </p>
+        </div>
+        <hr>
+        <div style="padding: 10px">
+            <p style="text-align: left; padding-bottom: 10px; font-weight: bold"><a href="javascript:goTo('distributionsAndFamilyLiquidity')">Distributions and Family Liquidity</a></p>
+            <p>Learn more about how we help our clients create policies for distributing income, transferring and redeeming shares and communicating return expectations.
+            </p>
+        </div>
+        <hr>
+        <div style="padding: 10px">
+            <p style="text-align: left; padding-bottom: 10px; font-weight: bold"><a href="javascript:goTo('balancingCompetingInterests')">Balancing Competing Interests</a></p>
+            <p>Learn more about how we help our clients create frameworks and processes for balancing competing interests within their businesses.
+            </p>
+        </div>
     </div>
     <div class="five columns">
         @include('web.private-banking._sidebar')
