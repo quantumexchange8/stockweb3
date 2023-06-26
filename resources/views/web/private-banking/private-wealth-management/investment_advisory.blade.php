a34216fc4aeb2ae99ea3d345716d96f0fb416a27
Irfan Zulkarnain <irfanzulkarnain.dev@yahoo.com>
2023-06-26 15:54:17 +0800

    style: add lang phrases

--- resources/views/web/private-banking/private-wealth-management/investment_advisory.blade.php
+++ resources/views/web/private-banking/private-wealth-management/investment_advisory.blade.php
@@ -15,29 +15,30 @@
     <div class="eleven columns">
         <h2>@lang('public.investment advisory')</h2>
         <p style="padding-top: 10px;">
-            Asset allocation is a crucial component of your investment strategy. We assist you in allocating your assets in accordance with your expenditure requirements, desire to leave wealth to future generations, and philanthropic goals. The better we comprehend the intended uses of your wealth, the more precisely we can align the risk and expected return characteristics of your portfolio with your objectives.
+            @lang('public.investment advisory 1')
+            <br><br>
+            @lang('public.investment advisory 2')
             <br><br>
 
-            We implement your asset allocation using a combination of in-house and third-party investment strategies centred on a single objective: preserving and expanding your wealth. All investment managers compete for inclusion on our platform, and to be considered, they must share our beliefs that purchasing securities at a discount to their valuation and understanding what you own and why are the most effective methods to preserve and grow wealth over time. <br><br>
+            <strong>@lang('public.investment advisory 3')</strong><br><br>
 
-            <strong>Asset Allocation Methodology</strong><br><br>
+            @lang('public.investment advisory 4')
+            <br><br>
 
-            The investment teams at Vanward provide asset allocation recommendations for a variety of client types, and our relationship teams use these recommendations as a starting point to combine our best thinking with investment and wealth planning advice tailored to your family's requirements. <br><br>
+            <strong>@lang('public.investment advisory 5')</strong><br>
+            &bull;&nbsp;<strong>@lang('public.investment advisory 6')</strong>: @lang('public.investment advisory 7') <br>
 
-            <strong>Our Fundamental Investment Principles</strong><br>
-            &bull;&nbsp;<strong>Active Administration</strong>: We believe that all markets are inherently inefficient and that investors can generate excess returns with less risk of permanent capital loss through careful security selection. <br>
+            &bull;&nbsp;<strong>@lang('public.investment advisory 8')</strong>: @lang('public.investment advisory 9') <br>
 
-            &bull;&nbsp;<strong>Bottom-Up Fundamental Investigations</strong>: We invest in strategies that forecast the value of each prospective investment through rigorous fundamental analysis. <br>
+            &bull;&nbsp;<strong>@lang('public.investment advisory 10')</strong>: @lang('public.investment advisory 11') <br>
 
-            &bull;&nbsp;<strong>Value Orientation</strong>: We pursue strategies that invest in securities trading below our estimate of their intrinsic value,* providing a margin of safety for the portfolio.** <br>
+            &bull;&nbsp;<strong>@lang('public.investment advisory 12')</strong>: @lang('public.investment advisory 13') <br>
 
-            &bull;&nbsp;<strong>Long-Term View</strong>: We believe that the most reliable method to generate attractive investment returns is to avoid being swayed by short-term market volatility while patiently awaiting the realisation of an investment's value. <br>
+            &bull;&nbsp;<strong>@lang('public.investment advisory 14')</strong>: @lang('public.investment advisory 15') <br>
 
-            &bull;&nbsp;<strong>Unique View of Risk</strong>: We believe that the greatest peril to your financial health is the permanent loss of capital, not market volatility. We believe that market volatility presents opportunities to acquire high-quality assets at a discount to their market value. <br>
+            &bull;&nbsp;<strong>@lang('public.investment advisory 16')</strong>: @lang('public.investment advisory 17') <br><br>
 
-            &bull;&nbsp;<strong>Discipline and Tenacity</strong>: We are patient investors. When nothing meets our investment criteria, we hold cash, and we have the conviction to sell when an investment no longer provides acceptable returns relative to its risk. <br><br>
-
-            *Based on our analysis of both tangible and ethereal factors, intrinsic value represents what we believe to be the value of a security. **A margin of safety exists when we believe there is a substantial discount to intrinsic value at the time of purchase – we strive to purchase at or below 75% of our estimate of intrinsic value. <br><br>
+            @lang('public.investment advisory 18')<br><br>
 
             <strong>NOT FDIC INSURED&nbsp;&nbsp;&nbsp;NO BANK GUARANTEE&nbsp;&nbsp;&nbsp;MAY LOSE VALUE</strong>
         </p>
